b3e989fff7099833e0d9b62a6709de12e7db8d41
Lucas Tavares <rtavares.lucas@gmail.com>
2020-06-08 10:27:35 -0300

    findByIdIMplemantation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,14 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
-		System.out.println(obj);
+		
 		
 		
 		
